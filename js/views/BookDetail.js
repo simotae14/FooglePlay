6097dd929bd0a6e898c609b0c8cbc9b7429d9d15
Simone Taeggi <startae14@gmail.com>
2018-02-25 19:44:21 +0100

    Underscore templating
    aggiunto del template con Underscore invece degli stringoni

--- js/views/BookDetail.js
+++ js/views/BookDetail.js
@@ -1,32 +1,17 @@
 app.views.BookDetail = Backbone.View.extend({
+	template: _.template($('script[data-id=book-detail-view]').html()),
 	initialize: function() {
 		this.listenTo(this.model, "change", this.render);
 	},
 	render: function() {
 		var info = this.model.get('volumeInfo');
 		var images = info.imageLinks;
-		this.$el.html(
-			'<header class="book-header l-content l-content-constrained l-row">' +
-				'<div class="l-column thumb-image">' +
-					'<img src="' + (images.small || images.thumbnail) + '">' +
-				'</div>' +
-				'<div class="l-column l-pad">' +
-					'<div class="title">' +
-						info.title +
-					'</div>' +
-					'<div>' +
-						(info.authors ? ('<em>' + info.authors.join(" - ") + '</em> - ') : '') + info.publishedDate + '<br>' +
-						(info.publisher ? (info.publisher + ' - Publisher') : '') +
-					'</div>' +					
-				'</div>' +
-			'</header>' +
-			'<div class="book-content l-content l-content-constrained standout">' +
-				'<h1 class="title">Descrizione</h1>' +
-				'<p>' +
-					info.description +
-				'</p>' +
-			'</div>'
-		);
+
+		this.$el.html(this.template({
+			info: info,
+			images: images
+		}));
+
 		return this;
 	}
 });
